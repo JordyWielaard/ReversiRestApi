022be25d49e8afa4ea95ecf21ef6ea8fd29dc5a5
Jordy Wielaard <jordy.wielaard@windesheim.nl>
2021-03-22 10:13:21 +0100

    Commit for test

---
+++ Create dbo.Cell.sql
@@ -0,0 +1,18 @@
+﻿USE [ReversiDbRestApi]
+GO
+
+/****** Object: Table [dbo].[Cell] Script Date: 3/22/2021 9:57:52 AM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[Cell] (
+    [Token] UNIQUEIDENTIFIER NOT NULL,
+    [Row]   INT              NOT NULL,
+    [Col]   INT              NOT NULL,
+    [Kleur] INT              NOT NULL
+);
+
+
---
+++ Create dbo.Games.sql
@@ -0,0 +1,22 @@
+﻿USE [ReversiDbRestApi]
+GO
+
+/****** Object: Table [dbo].[Games] Script Date: 3/22/2021 9:58:36 AM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[Games] (
+    [ID]           INT              IDENTITY (1, 1) NOT NULL,
+    [Token]        UNIQUEIDENTIFIER NULL,
+    [Speler1Token] UNIQUEIDENTIFIER NULL,
+    [Speler2Token] UNIQUEIDENTIFIER NULL,
+    [Omschrijving] VARCHAR (255)    NULL,
+    [AandeBeurt]   INT              NULL,
+    [Winnaar]      INT              NULL,
+    [Afgelopen]    BIT              NULL
+);
+
+
