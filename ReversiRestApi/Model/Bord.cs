b8125d0a0dc65f83fc010f3edb3d5a255856c221
Jordy Wielaard <jordy.wielaard@windesheim.nl>
2021-03-22 09:57:02 +0100

    Commit dit

---
+++ ReversiRestApi/Model/Bord.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ReversiRestApi.Model
+{
+    public class Bord
+    {
+        public string Token { get; set; }
+        public int Row { get; set; }
+        public int Col { get; set; }
+        public string Kleur { get; set; }
+    }
+}
