5bd6867bd70f5e8defd0c696f643091a00a6556e
Jordy Wielaard <jordy.wielaard@windesheim.nl>
2021-03-22 10:17:27 +0100

    Commit for merge

---
+++ ReversiRestApi/Data/SpelDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ReversiRestApi.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ReversiRestApi.Data
+{
+    public class SpelDbContext : DbContext
+    {
+        public SpelDbContext(DbContextOptions<SpelDbContext> options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Spel>(s =>
+            {
+                s.HasKey(s => s.ID);
+                s.Property(s => s.ID).ValueGeneratedOnAdd();
+            });
+        }
+        public DbSet<Spel> Spel { get; set; }
+    }
+}
